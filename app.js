c24cb910177f17f98d80ce83e90f4b44d523168a
floriceligonz <floriruiz77@gmail.com>
2025-08-06 15:58:53 -0600

    Actualizacion del app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    let amigosInput = document.getElementById("amigo").value;
+    if (amigosInput === "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(amigosInput);
+        console.log(amigos);
+        document.getElementById("amigo").value = "";
+        mostrarAmigos();
+    }
+}
+
+function mostrarAmigos() {
+    let listaInput = document.getElementById("listaAmigos");
+    listaInput.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        listaInput.innerHTML += `<li>${amigos[i]}</li>`;
+        console.log(amigos[i]);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("No hay amigos agregados o no hay suficientes amigos para sortear.");
+        return;
+    } else {
+        let aleatorio = Math.floor(Math.random() * amigos.length);
+        let sorteo = amigos[aleatorio];
+        document.getElementById("resultado").innerHTML = `El amigo secreto es: ${sorteo}`;
+    }
+}
